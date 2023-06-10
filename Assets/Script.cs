fca0d6a9e3291eca2b2a7033f68497f8f3f320f3
Иван <114279753+ivansvesh@users.noreply.github.com>
2023-06-10 10:32:34 +0400

    Initial commit

---
+++ Assets/Script.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script : MonoBehaviour
+{
+    public bool isdead = false;
+    public float timeRemaining = 0.1f;
+    private IEnumerator coroutine;
+    [SerializeField] private GameObject kol;
+    [SerializeField] private GameObject Target; 
+    GameObject objectTarget; 
+    MeshRenderer redMesh;    
+    [SerializeField] Material redMaterial, greenMaterial;
+    Material kolMaterial;
+    [SerializeField] private GameObject[] itemsArray;
+    [SerializeField] private GameObject itemsParent;
+ 
+    private int _totalItems = 0;
+    
+    void Start()
+    {
+        _totalItems = itemsParent.transform.childCount;
+ 
+        itemsArray = new GameObject[_totalItems];
+ 
+        for(int i = 0; i < _totalItems; i++)
+        {
+            itemsArray[i] = itemsParent.transform.GetChild(i).gameObject;
+        }
+         coroutine = СhangeСolorsTrafficLight();
+         StartCoroutine(coroutine);
+    }
+    private IEnumerator СhangeСolorsTrafficLight()
+	{  
+	    redMesh.material = redMaterial;
+	    yield return new WaitForSeconds(1f);
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {	if(collision.relativeVelocity.magnitude >= 3)
+	    {
+ 	        Destroy(gameObject);
+
+            objectTarget = collision.gameObject;
+            if (objectTarget == Target) 
+            {
+                kolMaterial = greenMaterial;
+            }
+            else
+            {
+                kolMaterial = redMaterial;
+            }
+
+	        GetComponent<Rigidbody>().isKinematic = false;
+	            foreach(GameObject i in itemsArray)
+	            {
+	                i.GetComponent<MeshRenderer>().material = kolMaterial; 
+  	                i.GetComponent<Rigidbody>().isKinematic = false;
+	            }
+
+              Vector3 pos = gameObject.transform.position;
+              isdead = true;
+              Instantiate(kol, pos, Quaternion.identity);
+        }
+    }
+
+    void Update()
+    {
+	Debug.Log(Resources.Load<GameObject>("kol") as GameObject);
+	if(isdead)
+        {
+	        timeRemaining -= Time.deltaTime;
+	        if(timeRemaining < 0)
+	        {
+	            Destroy(gameObject); 
+	        }
+        }
+    }
+}
---
+++ Assets/destroedMan.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class destroedMan : MonoBehaviour
+{
+    public bool isdead = false;
+    public float timeRemaining = 0.1f;
+    [SerializeField] Text CountGoal;
+    [SerializeField] private GameObject mon;
+    [SerializeField] private GameObject goal;
+    // Переменные для хранения MeshRenderer'a сигналов светофора
+   [SerializeField] MeshRenderer redLightMesh;
+     // Переменные для хранения материалов
+  [SerializeField] float switchingSpeed = 2f;
+     // IEnumerator для смены цветов
+     private IEnumerator coroutine;     
+	Material monLightMaterial;
+     [SerializeField] Material redLightMaterial;
+     [SerializeField] Material greenLightMaterial;
+  [SerializeField] private GameObject[] itemsArray;
+    [SerializeField] private GameObject itemsParent;
+GameObject gameObjec1;
+ private int _totalItems = 0;
+	private int goalcount;
+    
+    // Start is called before the first frame update
+    void Start()
+    {	
+        _totalItems = itemsParent.transform.childCount;
+ 
+        itemsArray = new GameObject[_totalItems];
+ 
+        for(int i = 0; i < _totalItems; i++)
+        {
+            itemsArray[i] = itemsParent.transform.GetChild(i).gameObject;
+        }
+        //GetComponent<Rigidbody>().isKinematic = true;
+         // Присваиваем переменной coroutine, IEnumerator СhangeСolorsTrafficLight
+         coroutine = СhangeСolorsTrafficLight();
+         // Запускаем IEnumerator СhangeСolorsTrafficLight
+         StartCoroutine(coroutine);
+    }
+    private IEnumerator СhangeСolorsTrafficLight()
+	{  
+	    
+	    redLightMesh.material = redLightMaterial;
+	    yield return new WaitForSeconds(1f * switchingSpeed);
+	    redLightMesh.material = greenLightMaterial;
+
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {	
+	if(collision.relativeVelocity.magnitude >= 4)
+	{ 
+
+	  Debug.Log("Cтолкновение.");
+	  gameObjec1 = collision.gameObject;
+	  if (gameObjec1 == goal) {
+	 goalcount =  int.Parse(CountGoal.text);
+	 goalcount++;
+	CountGoal.text = goalcount.ToString();
+	 monLightMaterial =  redLightMaterial; }
+	else {
+	monLightMaterial = greenLightMaterial;	}
+	GetComponent<Rigidbody>().isKinematic = false;
+	foreach(GameObject i in itemsArray)
+	{
+	    if(i.name != "Cylinder")
+		{
+	    i.GetComponent<MeshRenderer>().material =  monLightMaterial ;
+  	    i.GetComponent<Rigidbody>().isKinematic = false;
+	    print(i.name);
+		}
+	}
+	Destroy(gameObject);
+      Vector3 pos = gameObject.transform.position;
+      isdead = true;
+      Instantiate(mon,pos, Quaternion.identity);
+	}
+    }
+    // Update is called once per frame
+    void Update()
+    {
+	if(isdead)
+	{
+	  timeRemaining -= Time.deltaTime;
+	  if(timeRemaining < 0)
+	  {
+	    Destroy(gameObject); 
+	  }
+	}
+    }
+}
